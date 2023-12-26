152fa5459bfaf3960d589715dc607e7e5e39add1
Jonathan Jacka <jonathanjacka@gmail.com>
2023-12-25 19:20:24 -0600

    Adds basic form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 function App() {
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const input = e.currentTarget.elements.namedItem('text-area') as HTMLTextAreaElement
+    const output = e.currentTarget.nextElementSibling as HTMLPreElement
+    output.textContent = input.value
+  }
+
   return (
     <div>
-      Hello World!
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="text-area">Input:</label>
+        </div>
+        <textarea name="text-area" id="text-area" />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <pre>Poo</pre>
     </div>
+
   )
 }
 
