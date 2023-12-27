cd6eb3b5e3fff064922029685e43fd0545c14e42
Jonathan Jacka <jonathanjacka@gmail.com>
2023-12-27 06:32:41 -0600

    Updates unpkg plugin to handle relative paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,13 +10,19 @@ export const unpkgPathPlugin = () => {
 
         if(args.path === 'index.js') {
             return { path: args.path, namespace: 'a' };
-        } else if (args.path === 'tiny-test-pkg') {
+        } 
+
+        if(args.path.includes('./') || args.path.includes('../')) {
             return {
                 namespace: 'a',
-                path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js'
-            }
-
+                path: new URL(args.path, args.importer + '/').href
+            };
         }
+        
+        return {
+            namespace: 'a',
+            path: `https://unpkg.com/${args.path}`
+        };
         
       });
  
@@ -27,7 +33,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
