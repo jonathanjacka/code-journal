11ebcd872f142ffd7855c2b548d81416677cfa50
Jonathan Jacka <jonathanjacka@gmail.com>
2023-12-30 07:40:49 -0600

    Updates props for CodeEditor component

--- src/components/Code-editor.tsx
+++ src/components/Code-editor.tsx
@@ -3,16 +3,20 @@ import Editor from "@monaco-editor/react";
 interface CodeEditorProps {
     height?: string,
     defaultLanguage?: string,
-    defaultValue?: string
+    defaultValue?: string,
+    darkMode?: boolean
+
   }
 
-const CodeEditor = ({height, defaultLanguage, defaultValue}: CodeEditorProps) => {
+const CodeEditor = ({height, defaultLanguage, defaultValue, darkMode}: CodeEditorProps) => {
   return (
     <div>
       <Editor 
         height={height ? height :'500px'} 
         defaultLanguage={defaultLanguage ? defaultLanguage : 'javascript'} 
-        defaultValue={defaultValue ? defaultValue : "// write some javascript code..."}/>
+        defaultValue={defaultValue ? defaultValue : "// write some javascript code..."}
+        theme={darkMode === false ? 'light' : 'vs-dark'}
+        />
     </div>
   );
 };
