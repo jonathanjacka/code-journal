194101e8ae03d375456dd1dbb633bdf7a1f0f94e
Jonathan Jacka <jonathanjacka@gmail.com>
2023-12-30 12:57:24 -0600

    Updates options for code editor display

--- src/components/Code-editor.tsx
+++ src/components/Code-editor.tsx
@@ -5,17 +5,28 @@ interface CodeEditorProps {
     defaultLanguage?: string,
     defaultValue?: string,
     darkMode?: boolean
-
   }
 
 const CodeEditor = ({height, defaultLanguage, defaultValue, darkMode}: CodeEditorProps) => {
   return (
     <div>
-      <Editor 
+      <Editor
         height={height ? height :'500px'} 
         defaultLanguage={defaultLanguage ? defaultLanguage : 'javascript'} 
         defaultValue={defaultValue ? defaultValue : "// write some javascript code..."}
         theme={darkMode === false ? 'light' : 'vs-dark'}
+        options={
+            {
+                wordWrap: 'on',
+                minimap: { enabled: false },
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+                automaticLayout: true
+            }
+        }
         />
     </div>
   );
