70b5b13805313a7cac606281f9822a4ddad2c4ca
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-01 06:47:26 -0600

    Refactor for bundler import

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,20 @@
-import { useState, useEffect, useRef } from 'react';
-import esbuild from 'esbuild-wasm';
-
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
+import { useState } from 'react';
 
 import CodeEditor from './components/Code-editor';
 import Preview from './components/Preview';
+
+import bundler from './bundler';
 
 import 'bulmaswatch/united/bulmaswatch.min.css';
 
 function App() {
 
-  const serviceInitialized = useRef(false);
-
   const [code, setCode] = useState<string>('');
   const [input, setInput] = useState<string>('')
 
-  const startService = async () => {
-    await esbuild.initialize({
-      wasmURL: 'https://unpkg.com/esbuild-wasm/esbuild.wasm'
-    })
-  }
-
-  useEffect(() => {
-    if(!serviceInitialized.current) {
-      startService();
-      serviceInitialized.current = true;
-    }
-  }, []);
-
   const onSubmit = async () => {
-    if(!serviceInitialized.current) {
-      return;
-    }
-
-    const result = await esbuild.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    });  
-    setCode(result.outputFiles[0].text);
+    const output = await bundler(input);
+    setCode(output);
   }
 
   return (
---
+++ src/bundler/index.ts
@@ -0,0 +1,31 @@
+import esbuild from 'esbuild-wasm';
+
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
+import { fetchPlugin } from '../plugins/fetch-plugin';
+
+let startService: boolean = false;
+
+export default async (inputCode: string) => {
+    if(!startService){
+        await esbuild.initialize({
+            wasmURL: 'https://unpkg.com/esbuild-wasm/esbuild.wasm'
+          })
+        startService = true;
+    }
+
+    const result = await esbuild.build({
+        entryPoints: ['index.js'],
+        bundle: true,
+        write: false,
+        plugins: [
+          unpkgPathPlugin(),
+          fetchPlugin(inputCode)
+        ],
+        define: {
+          'process.env.NODE_ENV': '"production"',
+          global: 'window'
+        }
+      }); 
+
+      return result.outputFiles[0].text;
+}
