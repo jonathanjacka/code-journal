ed1d4b3978e7de220dccdf1123958c2eb8974e08
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-05 06:39:10 -0600

    Adds addCell component

---
+++ src/components/Add-cell.tsx
@@ -0,0 +1,20 @@
+import { useActions } from '../hooks/useActions';
+
+import './Add-cell.css';
+
+interface AddCellProps {
+    nextCellId: string | null;
+}
+
+const AddCell: React.FC<AddCellProps> = ( {nextCellId} ) => {
+    const { insertCellBefore } = useActions();
+
+    return (
+        <div>
+            <button onClick={() => insertCellBefore(nextCellId, 'code')}>Code</button>
+            <button onClick={() => insertCellBefore(nextCellId, 'text')}>Text</button>
+        </div>
+    )
+}
+
+export default AddCell;
--- src/components/Cell-list.tsx
+++ src/components/Cell-list.tsx
@@ -1,4 +1,8 @@
 import { useTypedSelector } from '../hooks/useTypedSelector';
+import { Fragment } from 'react';
+
+import AddCell from './Add-cell';
+
 import CellListItem from './Cell-list-item';
 
 const CellList: React.FC = () => {
@@ -6,11 +10,18 @@ const CellList: React.FC = () => {
     const { data, order } = useTypedSelector((state) => state.cells);
     const cells = order.map((id) => data[id]);
 
-    const renderedCells = cells.map(cell => <CellListItem key={cell.id} cell={cell}/>);
+    const renderedCells = cells.map(cell => 
+      <Fragment key={cell.id}>
+        <AddCell nextCellId={cell.id} />
+        <CellListItem cell={cell} />
+      </Fragment>
+      
+    );
 
   return (
     <div>
       {renderedCells}
+      <AddCell nextCellId={null} />
     </div>
   )
 }
