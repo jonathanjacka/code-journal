4d306b9dd21b28ddbd885eab9e4b90f6951df3fb
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-08 15:17:24 -0600

    Adds show function to code cell

--- src/components/Code-cell.tsx
+++ src/components/Code-cell.tsx
@@ -27,7 +27,24 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = useCallback(() => {
         const orderedCells = order.map(id => data[id]);
-        const cumulativeCode = [];
+        const cumulativeCode = [
+            `
+                import _React from 'react';
+                import _ReactDOM from 'react-dom';
+                const show = (value) => {
+                    const root = document.querySelector('#root');
+                    if(typeof value === 'object'){
+                        if(value.$$typeof && value.props){
+                            _ReactDOM.render(value, root);
+                        }else{
+                            root.innerHTML = JSON.stringify(value);
+                        }
+                    }else{
+                        root.innerHTML = value;
+                    }
+                }
+            `
+        ];
         for(const c of orderedCells){
             if(c.type === 'code'){
                 cumulativeCode.push(c.content);
