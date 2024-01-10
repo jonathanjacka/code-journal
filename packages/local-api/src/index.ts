5635b8d891c3adc78ca58aa73af389348abf5ad4
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-10 13:47:44 -0600

    Updates cli and local-api connection

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-    console.log('Hi there! Your server is running.');
+export const serve = (port: number, filename: string, dir: string) => {
+    console.log(`Hi there! Your server is running on port ${port}`);
+    console.log(`You're saving and fetching cells from ${filename}`);
+    console.log(`This file is located in the following directory ${dir}`);
 }
