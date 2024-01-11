627590af889c68602270a2d1aeccb10356ec61af
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-11 10:39:27 -0600

    Adds proxy for web app

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import chalk from 'chalk';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const PROXY_PORT = 5173;
 
 export const serve = (port: number, filename: string, dir: string) => {
 
     const app = express();
+
+    app.use(createProxyMiddleware({
+        target: `http://localhost:${PROXY_PORT}`,
+        ws: true,
+        logLevel: 'silent'
+    }));
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, () => {
