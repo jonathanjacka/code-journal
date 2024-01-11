d80dac5d2350db1eaa3c11d891d4642b2ed706e1
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-11 15:42:42 -0600

    Updates package connections for dev and prod

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,8 +1,9 @@
 import path from 'path';
 import { Command } from 'commander';
 import { serve } from 'local-api';
-
 import chalk from 'chalk';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const serveCommand = new Command()
     .command('serve [filename]')
@@ -11,7 +12,7 @@ export const serveCommand = new Command()
     .action( async (filename = 'notebook.js', options: { port: string }) => {
         try {
             const dir = path.join(process.cwd(), path.dirname(filename));
-            await serve(parseInt(options.port), path.basename(filename), dir);
+            await serve(parseInt(options.port), path.basename(filename), dir, !isProduction);
             console.log(chalk.green(`Successfully opened ${filename}. Navigate to ` + chalk.blue.underline(`http://localhost:${options.port}`) + ` to edit the file!`));
 
         } catch (error: any) {
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,18 +1,30 @@
 import express from 'express';
+const path = require('path');
 import chalk from 'chalk';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
+
 const PROXY_PORT = 5173;
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+    port: number, 
+    filename: string, 
+    dir: string, 
+    useDevProxy: boolean
+    ) => {
 
     const app = express();
 
-    app.use(createProxyMiddleware({
-        target: `http://localhost:${PROXY_PORT}`,
-        ws: true,
-        logLevel: 'silent'
-    }));
+    if(useDevProxy) {
+        app.use(createProxyMiddleware({
+            target: `http://localhost:${PROXY_PORT}`,
+            ws: true,
+            logLevel: 'silent'
+        }));
+    } else {
+        const packagePath = require.resolve('local-client/dist/index.html');
+        app.use(express.static(path.dirname(packagePath)));
+    }
 
     return new Promise<void>((resolve, reject) => {
         app.listen(port, () => {
