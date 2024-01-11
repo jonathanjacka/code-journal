0e4f3ce297cfdcdf58ecb8f82660bd92d812418e
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-11 05:52:36 -0600

    Adds some error handling to serve command

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -5,8 +5,10 @@ export const serve = (port: number, filename: string, dir: string) => {
 
     const app = express();
 
-    app.listen(port, () => {
-        console.log(chalk.bgBlue(`Listening on port ${port}`));
+    return new Promise<void>((resolve, reject) => {
+        app.listen(port, () => {
+            console.log(chalk.bgBlue(`Local server is now listening on port ${port}`));
+            resolve();
+        }).on('error', reject);
     });
-    
 }
