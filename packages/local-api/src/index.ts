490df70c976809669315124707121e943f65306b
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-11 04:44:26 -0600

    Adds express to local-api

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,5 +1,12 @@
+import express from 'express';
+import chalk from 'chalk';
+
 export const serve = (port: number, filename: string, dir: string) => {
-    console.log(`Hi there! Your server is running on port ${port}`);
-    console.log(`You're saving and fetching cells from ${filename}`);
-    console.log(`This file is located in the following directory ${dir}`);
+
+    const app = express();
+
+    app.listen(port, () => {
+        console.log(chalk.bgBlue(`Listening on port ${port}`));
+    });
+    
 }
