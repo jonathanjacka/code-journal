db1e19c0e21474d485684d31f731f82f70432ca5
Jonathan Jacka <jonathanjacka@gmail.com>
2024-01-12 14:11:26 -0600

    Adds save cells middleware to cells reducer/store

--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import reducers from './reducers';
+import { persistMiddleware } from './middleware/persist-middleware';
 
 export const store = createStore(
     reducers,
     {},
-    applyMiddleware(thunk)
+    //@ts-expect-error - action type is not a string
+    applyMiddleware(thunk, persistMiddleware)
 );
 
 
